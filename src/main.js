f52abb69d26c05f6630a0d5157bbb9671a21d305
sheilamr <sheila.24.1996@gmail.com>
2026-04-28 16:36:07 +0200

    version comandero

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import { createI18n } from "vue-i18n";
+import { version } from "../package.json";
 require("./services/sockets");
 axios.defaults.baseURL = "http://" + window.location.hostname + ":3000";
+
+// Report version to backend
+axios.post("getInfo/actualizarVersionComandero", { version }).catch(() => {
+  /* Silent error */
+});
+
 const i18n = createI18n({
   legacy: false,
   locale: "es",
